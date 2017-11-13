cedd915ae3ad90dac2fc975cf3973570c198f398
samratshekhar <samratshekhar42@gmail.com>
2017-11-13 20:32:09 +0530

    Fix build for RN 0.48 (#94)
    https://github.com/yonahforst/react-native-permissions/issues/137

--- RNCalendarEvents.h
+++ RNCalendarEvents.h
@@ -1,4 +1,8 @@
-#import <React/RCTBridgeModule.h>
+#if __has_include(<React/RCTBridgeModule.h>)
+  #import <React/RCTBridgeModule.h>
+#else
+  #import "RCTBridgeModule.h"
+#endif
 #import <React/RCTEventDispatcher.h>
 
 @interface RNCalendarEvents : NSObject <RCTBridgeModule>
