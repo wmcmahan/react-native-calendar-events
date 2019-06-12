e1e0aef84d20a3b04c089b733a5329b5a0f676e0
eladgel <elad@honeybook.com>
2019-06-12 22:56:28 +0300

    Update RNCalendarEvents.m
    Dont add structured location if no title and no radius

--- RNCalendarEvents.m
+++ RNCalendarEvents.m
@@ -655,7 +655,7 @@ RCT_EXPORT_MODULE()
 
     [formedCalendarEvent setValue:[self availabilityStringMatchingConstant:event.availability] forKey:_availability];
     
-    if (event.structuredLocation) {
+    if (event.structuredLocation && event.structuredLocation.title && event.structuredLocation.radius) {
         NSMutableDictionary *structuredLocation = [[NSMutableDictionary alloc] initWithCapacity:3];
         [structuredLocation addEntriesFromDictionary: @{
                                                         @"title": event.structuredLocation.title,
