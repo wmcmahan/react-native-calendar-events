f7c3db41b725ebc159fd2dde0cd3e21c4b4c48d6
Maxime Thirouin <git@moox.io>
2020-07-27 20:55:11 +0200

    Add example (react-native-init)

---
+++ example/App.js
@@ -0,0 +1,139 @@
+/**
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+  Alert,
+} from 'react-native';
+import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
+import RNCalendarEvents from 'react-native-calendar-events';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Auth</Text>
+              <Text style={styles.sectionDescription}>
+                <Button
+                  title="Request auth"
+                  onPress={() => {
+                    RNCalendarEvents.authorizeEventStore().then(
+                      (result) => {
+                        Alert.alert('Auth requested', result);
+                      },
+                      (result) => {
+                        console.error(result);
+                      },
+                    );
+                  }}
+                />
+                <Text>{'\n'}</Text>
+                <Button
+                  title="Check auth"
+                  onPress={() => {
+                    RNCalendarEvents.authorizationStatus().then(
+                      (result) => {
+                        Alert.alert('Auth check', result);
+                      },
+                      (result) => {
+                        console.error(result);
+                      },
+                    );
+                  }}
+                />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Calendars</Text>
+              <Text style={styles.sectionDescription}>
+                <Button
+                  title="Find calendars"
+                  onPress={() => {
+                    RNCalendarEvents.findCalendars().then(
+                      (result) => {
+                        Alert.alert(
+                          'Calendars',
+                          result
+                            .reduce((acc, cal) => {
+                              acc.push(cal.title);
+                              return acc;
+                            }, [])
+                            .join('\n'),
+                        );
+                      },
+                      (result) => {
+                        console.error(result);
+                      },
+                    );
+                  }}
+                />
+              </Text>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
