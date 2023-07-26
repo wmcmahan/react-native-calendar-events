3ed9c44eae83c2bb6fa11c5cdb0964ebf47eda0f
Nolan Cong <nolan@thisapp.so>
2023-07-26 00:26:06 -0700

    add status types

--- src/index.d.ts
+++ src/index.d.ts
@@ -48,6 +48,22 @@ interface RecurrenceRule {
   interval: number;
 }
 
+export enum AttendeeStatus {
+  IOSAccepted = "Accepted",
+  IOSCompleted = "Completed",
+  IOSDeclined = "Declined",
+  IOSDelegated = "Delegated",
+  IOSInProcess = "InProcess",
+  IOSPending = "Pending",
+  IOSTentative = "Tentative",
+  IOSUnknown = "Unknown",
+  AndroidNone = 0,
+  AndroidAccepted = 1,
+  AndroidDeclined = 2,
+  AndroidInvited = 3,
+  AndroidTentative = 4,
+}
+
 interface Attendee {
   /** The name of the attendee. */
   name: string;
